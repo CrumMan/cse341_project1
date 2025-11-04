8619029f7b2b24f64b1d156760af10a9a39d5669
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-11-03 22:56:27 -0600

    playing with database to get it to run on render

--- data/database.js
+++ data/database.js
@@ -8,9 +8,13 @@ const initDb = (callback) => {
    if (database){
     console.log('Db is already installed')
    } 
+   console.log('MONGODB_URI value:', process.env.MONGODB_URI);
+   console.log('MONGODB_URI type:', typeof process.env.MONGODB_URI);
+   console.log('First 20 chars:', process.env.MONGODB_URI ? process.env.MONGODB_URI.substring(0, 20) : 'undefined');
+   
    MongoClient.connect(process.env.MONGODB_URI)
    .then((client)=> {
-    database = client.db();
+    database = client.db;
     callback(null, database);
    })
    .catch((err) => {
