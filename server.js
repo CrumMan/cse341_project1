4339640f30b5696c3fde641a792689c8e514aa09
CrumMan <122419541+CrumMan@users.noreply.github.com>
2025-11-03 23:22:45 -0600

    please be done?

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json())
 app.use((req, res,next) => {
-    res.setHeader('Access-Control-Allow-Orgin', '*')
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader(
         "Access-Control-Allow-Headers",
         'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
@@ -18,13 +18,15 @@ app.use((req, res,next) => {
     next();
 })
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/', require('./routes'))
-
 
 mongodb.initDb((err) => {
-   if(err) console.log(err)
-    else{
+    if(err) console.log(err)
+        else{
+        
+        app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+        app.use('/', require('./routes'))
+
+
         app.listen(port, () => {console.log(`Database is listening and node is running on port ${port}`)} )
     }
 })
